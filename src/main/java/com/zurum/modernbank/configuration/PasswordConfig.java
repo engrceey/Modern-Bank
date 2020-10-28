a86ed25a42c039ea6b9a40a74f3cdeec7b834420
engrceey <ogbondachristian@gmail.com>
2020-10-28 09:50:00 +0100

    add password encryption

--- src/main/java/com/zurum/modernbank/configuration/PasswordConfig.java
+++ src/main/java/com/zurum/modernbank/configuration/PasswordConfig.java
@@ -1,12 +1,12 @@
 package com.zurum.modernbank.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
 
-@Component
-public class passwordConfig {
+@Configuration
+public class PasswordConfig {
 
     @Bean
     public PasswordEncoder encoder() {
